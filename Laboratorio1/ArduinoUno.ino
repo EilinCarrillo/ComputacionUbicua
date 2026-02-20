1d4d8e28cfd0b4a35eca05cfc0f6f4b9f5c74984
EilinCarrillo <eilincarrillo06@gmail.com>
2026-02-19 20:33:15 -0500

    Terminado

--- Laboratorio1/ArduinoUno.ino
+++ Laboratorio1/ArduinoUno.ino
@@ -1,11 +1,10 @@
 void setup(){
-  pinMode(9, OUTPUT);     
+  pinMode(13, OUTPUT);      
 }
 
 void loop(){
-  digitalWrite(9, HIGH);   
+  digitalWrite(13, HIGH);    
   delay(1000);               
-  digitalWrite(9, LOW);    
+  digitalWrite(13, LOW);     
   delay(1000);                
 }
-
